c80f248dd1764f669ce6919211088adbd4303f82
jaja <jaja@jaja.jaja>
2023-03-28 16:57:02 +0100

    go docker compose

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,23 +1,18 @@
 sleep 10
-if [ ! -f "/run/php"]
-then
-    mkdir /run/php
-    touch /run/php/php7.3-fpm.pid
-fi
 
 if [ -f "/var/www/wordpress/wp-config.php"];
 then
-    echo "wp-config.php already exist..."
+    echo "wp-config-sample.php already exist..."
 else
-    wget https://ra.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
     cd /var/www/wordpress
-    wp core download --allow-root
-    mv /var/www/wp-config.php /var/wwww/wordpress
+    wp config create --allow-root \
+                     --dbname=$SQL_DATABASE \
+                     --dbuser=$SQL_USER \
+                     --dbpass=$SQL_PASSWORD \
+                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
+fi
 
-    wp core install --url=jbouyer.42.fr/wordpress \
-    --title = Incption \
-    --admin_user=$ADMIN_USER \
-    --admin_password=$ADMIN_PASSWORD \
-    --admin_email = $ADMIN_EMAIL \
+#reste a faire la config de la deuxieme page automatique avec wp core install et ajouter un autre user aec la commande wp user create
+#aussi copier le fichier aut_confi.sh dans le continer et l'executer avec ENTRYPOINT
+#rajouter la secu avec la creation du dossier /run/php s'il n'existe pas.
+#et on lance php-fpm avec la commande /usr/sbin/php-fpm7.3 -F
