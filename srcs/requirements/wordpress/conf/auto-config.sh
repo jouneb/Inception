94be7deb5200e38da5db8a93d019ae1e7794c70b
jaja <jaja@jaja.jaja>
2023-04-05 14:24:47 +0100

    encore des pb a gerer

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,34 +1,33 @@
 sleep 10
 
 #on cree le dosier /run/php s'il n'existe pas pour permettre a php-fpm de fonctionner
-if [ ! -d "/run/php"]
+if [ ! -f "/run/php" ]
 then 
-   mkdir /run/php
+    mkdir /run/php
 fi
 
-if [ -f "/var/www/wordpress/wp-config.php"];
+if [ -f /var/www/wordpress/wp-config.php ];
 then
     echo "wp-config-sample.php already exist..."
 else
-    cd /var/www/wordpress
-    #on configure wp
+    wp core --allow-root download
+    #on configure wp et reation du wp-config.php
     wp config create --allow-root \
                      --dbname=$SQL_DATABASE \
                      --dbuser=$SQL_USER \
                      --dbpass=$SQL_PASSWORD \
-                     --dbhost=mariadb:3306 --path='/var/www/wordpress'
+                     --dbhost=mariadb:3306
    # on config la deuxieme page
-    wp core install --url=lmarecha.42.fr/wordpress \
+    wp core install --allow-root \
+                    --url=jbouyer.42.fr/wordpress \
                     --title=Inception \
                     --admin_user=$ADMIN_USER \
                     --admin_password=$ADMIN_PASSWORD \
-                    --admin_email==$ADMIN_EMAIL \
-                    --path=/var/www/wordpress \
-                    --allow-root
+                    --admin_email==$ADMIN_EMAIL 
+                    
     #on ajoute un user
 	wp user create $USER_NAME $USER_EMAIL \
 			--user_pass=$USER_PASSWORD \
-			--path=/var/www/wordpress \
 			--allow-root
 
 	chown -R www-data /var/www/wordpress
